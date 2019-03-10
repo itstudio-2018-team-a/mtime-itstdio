cab82c8c6faee188ae30596fe252d35170659dfb
CalunVier <CalunVier@outlook.com>
2019-03-11 00:58:23 +0800

    init

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\CalunVier\Documents\Development\python\mtime_itstdio\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
