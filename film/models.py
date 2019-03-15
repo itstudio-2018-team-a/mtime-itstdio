6d58c601eb63c7776f57533f79815ee48237b575
CalunVier <CalunVier@outlook.com>
2019-03-15 16:43:11 +0800

    完成数据库 添加film.models.OnMovie 添加film.models.ComingMovie 修改film.models.Film.info的最大长度为1024，即增加电影简介长度 setting中添加了account, news, film这三个应用 向.gitignore中添加了忽略migrations文件夹

--- film/models.py
+++ film/models.py
@@ -3,9 +3,17 @@ from django.db import models
 
 class Film(models.Model):
     title = models.CharField(max_length=40, verbose_name=u'电影标题')
-    info = models.CharField(max_length=255, verbose_name=u'电影简介')
+    info = models.CharField(max_length=1024, verbose_name=u'电影简介')
     public_time = models.DateTimeField(verbose_name=u'上映时间')
     score = models.IntegerField(default=-1, verbose_name=u'评分')
     marking_members = models.IntegerField(default=0, verbose_name=u'评分人数')
     poster = models.URLField(verbose_name=u'海报')
     commenting_members = models.IntegerField(default=0, verbose_name=u'评论数')
+
+
+class OnMovie(models.Model):
+    film_id = models.IntegerField(verbose_name=u'电影id')
+
+
+class ComingMovie(models.Model):
+    film_id = models.IntegerField(verbose_name=u'电影ID')
--- news/models.py
+++ news/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=255, verbose_name=u"新闻标题")
     author_id = models.IntegerField(verbose_name=u"作者ID(内部ID)")
+    # content 内容以文件的形式保存
     public_time = models.DateTimeField(auto_now=True, verbose_name=u"发表时间")
     view_times = models.IntegerField(verbose_name=u"浏览次数")
     hotpoint = models.IntegerField(verbose_name=u"热度")
