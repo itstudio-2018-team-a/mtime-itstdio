fb45f2c0aa7d6a078433ffcc28ba539613c0d29c
CalunVier <CalunVier@outlook.com>
2019-03-16 19:01:44 +0800

    删除所有__pycache__

--- account/models.py
+++ account/models.py
@@ -6,3 +6,6 @@ class MyUser(AbstractUser):
     email = models.EmailField(_('email address'),db_index=True,unique=True,verbose_name=u"电子邮件")
     nickname = models.CharField(max_length=20, verbose_name=u"昵称")
     head_image = models.URLField(verbose_name=u"头像URL")
+
+    class Meta:
+        indexes = [models.Index(['email']), models.Index]
