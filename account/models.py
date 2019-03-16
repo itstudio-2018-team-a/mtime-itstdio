c04a544a87e96a75d34a3c15d2e0b9192e1bfd4c
CalunVier <CalunVier@outlook.com>
2019-03-16 19:13:36 +0800

    删除MyUser中自定义的emmail，改用meta添加unique

--- account/models.py
+++ account/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class MyUser(AbstractUser):
-    email = models.EmailField(db_index=True,unique=True,verbose_name=u"电子邮件")
     nickname = models.CharField(max_length=20, verbose_name=u"昵称")
     head_image = models.URLField(verbose_name=u"头像URL")
 
     class Meta:
         indexes = [models.Index(['email', 'email']), models.Index(['username', 'username'])]
+        unique_together = ('email',)
