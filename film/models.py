21d72976850103e8d09fb8044c90381695e98577
CalunVier <CalunVier@outlook.com>
2019-03-16 22:20:50 +0800

    创建了三个表：电影评论、影评、影评评论

--- film/models.py
+++ film/models.py
@@ -9,3 +9,22 @@ class Film(models.Model):
     marking_members = models.IntegerField(default=0, verbose_name=u'评分人数')
     poster = models.URLField(verbose_name=u'海报')
     commenting_members = models.IntegerField(default=0, verbose_name=u'评论数')
+
+
+class FilmComment(models.Model):
+    film_id = models.IntegerField(db_index=True, verbose_name=u'电影ID')
+    author_id = models.IntegerField(db_index=True, verbose_name=u'作者ID')
+    content = models.CharField(max_length=1024, verbose_name=u'评论内容')
+
+
+class FilmReview(models.Model):
+    film_id = models.IntegerField(db_index=True, verbose_name=u'film_id')
+    author_id = models.IntegerField(db_index=True, verbose_name=u'作者ID')
+    # content = 内容存入文件
+    image = models.URLField(null=True, verbose_name=u'缩略图URL')
+
+
+class FilmReviewComment(models.Model):
+    film_review_id = models.IntegerField(db_index=True, verbose_name=u'影评ID')
+    author_id = models.IntegerField(db_index=True, verbose_name=u'作者ID')
+    content = models.CharField(max_length=1024, verbose_name=u'评论内容')
