bb69cb1f9115c64f16f45e3ccd701c09e19e2f02
CalunVier <CalunVier@outlook.com>
2019-03-29 12:54:28 +0800

    密码加密、login为数据不全的判断添加了else、复制粘贴创建了app_login

--- account/account_user.py
+++ account/account_user.py
@@ -1,5 +1,6 @@
 from .models import User
 from account.models import User
+import hashlib
 
 
 '''
@@ -17,3 +18,12 @@ def to_register(user_id, user_name, password, email):
         return 2
     User(username=user_id, password=password, email=email, nickname=user_name).save()
     return 0
+
+
+'''用于对密码进行MD5加密的函数'''
+
+
+def sign_password_md5(passwd, salt='kHa4sDk3dhQf'):
+    hashpwd_builder = hashlib.md5()         # 构建md5加密器
+    hashpwd_builder.update(passwd+salt)
+    return hashpwd_builder.hexdigest()      # 返回加密结果
